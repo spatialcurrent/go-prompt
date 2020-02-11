f0b7b4dc3ec1f8c7c838c7816850485bcce19630
pjdufour <pjdufour.dev@gmail.com>
2020-02-10 22:52:30 -0500

    fixes

--- cmd/goprompt/main.go
+++ cmd/goprompt/main.go
@@ -20,7 +20,6 @@
 package main
 
 import (
-	//"encoding/json"
 	"errors"
 	"fmt"
 	"os"
--- pkg/prompt/String_test.go
+++
@@ -1,8 +0,0 @@
-// =================================================================
-//
-// Copyright (C) 2020 Spatial Current, Inc. - All Rights Reserved
-// Released as open source under the MIT License.  See LICENSE file.
-//
-// =================================================================
-
-package prompt
